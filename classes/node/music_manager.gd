557722377e3d14cd96106493440a402f8017104e
clarence112 <clarencepacelli@gmail.com>
2026-04-19 06:12:37 -0400

    tweak: additional animation mode

--- classes/node/music_manager.gd
+++ classes/node/music_manager.gd
@@ -7,6 +7,7 @@ enum BeatMode {
 	EVERY,
 	ODD,
 	EVEN,
+	ALTERNATE,
 }
 
 
--- classes/node/music_synced_animation_player.gd
+++ classes/node/music_synced_animation_player.gd
@@ -4,6 +4,7 @@ extends AnimationPlayer
 
 @export var beat_mode:MusicManager.BeatMode
 @export var beat_animation:StringName
+@export var alternate_beat_animation:StringName
 
 
 func _ready() -> void:
@@ -11,14 +12,23 @@ func _ready() -> void:
 		assert(is_instance_valid(MusicManager.instance), "No MusicManager instance!")
 		match beat_mode:
 			MusicManager.BeatMode.EVERY:
-				MusicManager.instance.beat.connect(_trigger)
+				MusicManager.instance.beat.connect(_trigger_a)
 			MusicManager.BeatMode.EVEN:
-				MusicManager.instance.even_beat.connect(_trigger)
+				MusicManager.instance.even_beat.connect(_trigger_a)
 			MusicManager.BeatMode.ODD:
-				MusicManager.instance.odd_beat.connect(_trigger)
+				MusicManager.instance.odd_beat.connect(_trigger_a)
+			MusicManager.BeatMode.ALTERNATE:
+				MusicManager.instance.even_beat.connect(_trigger_a)
+				MusicManager.instance.odd_beat.connect(_trigger_b)
 	else:
 		assert(false, "Invalid Animation!")
 
 
-func _trigger() -> void:
+func _trigger_a() -> void:
+	stop()
 	play(beat_animation)
+
+
+func _trigger_b() -> void:
+	stop()
+	play(alternate_beat_animation)
